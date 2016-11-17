923e68022b937e4cb89aba706dae327dd8b5b3b0
Eric Barboni <skygo@apache.org>
2016-11-17 15:24:00 +0100

    ignore file

--- app/src/FortheHorde/Twig/Extensions/Text.php
+++ app/src/FortheHorde/Twig/Extensions/Text.php
@@ -2,6 +2,7 @@
 
 namespace FortheHorde\Twig\Extensions;
 
+use Twig_Environment;
 use Twig_Extension;
 use Twig_SimpleFunction;
 
@@ -9,16 +10,48 @@ class Text extends Twig_Extension {
 
     public function getFunctions() {
         $functions = array(
-            new Twig_SimpleFunction('showRoster', array($this, 'showGURoster'), array('needs_environment' => true, 'is_safe' => array('html'))),
+            new Twig_SimpleFunction('showMain', array($this, 'showGURoster'), array('needs_environment' => true, 'is_safe' => array('html'))),
+            new Twig_SimpleFunction('showAlts', array($this, 'showGURosterAlts'), array('needs_environment' => true, 'is_safe' => array('html'))),
         );
         return $functions;
     }
 
-    public function showGURoster() {
-        $cool = "ABC";
+    public function showGURoster(Twig_Environment $env) {
+
+        $path = realpath(__DIR__ . "/../../../../../app/_data/guild.json");
+
+        $myfile = fopen($path, "r") or die("Unable to open file!");
+        $json = fread($myfile, filesize($path));
+        fclose($myfile);
+        $cool = "";
+        $json_decoded = json_decode($json);
+        for ($rank = 0; $rank <= 1; $rank++) {
+            foreach ($json_decoded->members as $character) {
+                if ($character->rank == $rank) {
+                    $cool .= $character->character->name;
+                }
+            }
+        }
+        return $cool;
+    }
+
+    public function showGURosterAlts(Twig_Environment $env) {
+
+        $path = realpath(__DIR__ . "/../../../../../app/_data/guild.json");
+
+        $myfile = fopen($path, "r") or die("Unable to open file!");
+        $json = fread($myfile, filesize($path));
+        fclose($myfile);
+        $cool = "";
+        $json_decoded = json_decode($json);
+        for ($rank = 2; $rank < 10; $rank++) {
+            foreach ($json_decoded->members as $character) {
+                if ($character->rank == $rank) {
+                    $cool .= $character->character->name;
+                }
+            }
+        }
         return $cool;
     }
 
 }
-
-?>
