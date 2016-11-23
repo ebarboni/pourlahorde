4513040072ffabd2934eef24b8a6bd1705ccef8d
Eric Barboni <skygo@apache.org>
2016-11-23 14:44:51 +0100

    use pipeline

--- app/src/FortheHorde/Twig/Extensions/Text.php
+++ app/src/FortheHorde/Twig/Extensions/Text.php
@@ -245,7 +245,7 @@ class Text extends Twig_Extension {
 
     public function itemIcon(Twig_Environment $env, $id) {
         $cool = '';
-        $cool .= '<a href="//fr.wowhead.com/item='.$id.'" rel="item='.$id.'">ph</a>';
+        $cool .= '<a href="//fr.wowhead.com/item='.$id.'" rel="item='.$id.'"></a>';
 
         return $cool;
     }
