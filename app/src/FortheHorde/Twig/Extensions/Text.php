f37c558be4ffb74956dc2ccf30bcdabf34eee387
Eric <Eric@Eric-PC>
2017-01-18 01:17:44 +0100

    factoriez

--- app/src/FortheHorde/Twig/Extensions/Text.php
+++ app/src/FortheHorde/Twig/Extensions/Text.php
@@ -144,7 +144,7 @@ class Text extends Twig_Extension {
         $json = fread($myfile, filesize($path));
         fclose($myfile);
         $json_decoded = json_decode($json);
-        if (is_object($json_decoded)) {
+        if (is_object($json_decoded->professions)) {
             foreach ($json_decoded->professions as $typeofprof) {
                 foreach ($typeofprof as $prof) {
                     $trade[$prof->id][$name] = ["rank" => $prof->rank, "max" => $prof->max];
